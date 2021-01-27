6c369f0d8a0e5fe08731ae3ea3a5b5a1bd6bafe9
jazzo <jazzo@14.40.8.208>
2021-01-27 16:14:28 +0900

    add positionAdd jsp, css

--- Final_PJ/src/main/java/com/it/wanted/companyservice/controller/PositionController.java
+++ Final_PJ/src/main/java/com/it/wanted/companyservice/controller/PositionController.java
@@ -1,5 +1,6 @@
 package com.it.wanted.companyservice.controller;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -18,5 +19,9 @@ public class PositionController {
 	@RequestMapping("/positionAdd.do")
 	public void positionAdd() {
 		logger.info("포지션등록 페이지 조회");
+		
+		//직군 테이블 끌어오기
+		
+		//직무 테이블 끌어오기
 	}
 }
