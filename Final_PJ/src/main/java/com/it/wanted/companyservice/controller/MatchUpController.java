9b70a0a49714855d45c60b11a06559d637444f6a
jazzo <jazzo@14.40.8.208>
2021-01-28 14:35:05 +0900

    commit for pulling

--- Final_PJ/src/main/java/com/it/wanted/companyservice/controller/MatchUpController.java
+++ Final_PJ/src/main/java/com/it/wanted/companyservice/controller/MatchUpController.java
@@ -1,5 +1,7 @@
 package com.it.wanted.companyservice.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -19,7 +21,32 @@ public class MatchUpController {
 	
 	@RequestMapping("/matchupSearch.do")
 	public String matchupSearch() {
-		logger.info("기업서비스 매치업 메인화면");
+		logger.info("기업서비스 매치업 검색/조회화면");
+		
+		// [1] 직군리스트 세팅
+		
+		// [2] 전체 매치업이력서 불러오기, 이력서 공개여부 Y에 해당하는 
+		List<>
+		// [3] 이력서 번호에 해당하는 경력 (career) 불러와서 기간 계산하기
+		// 커리어VO 리스트 가져오기
+		
+		/*
+		 if(마감월 == 시작월){
+		 	경력 = 마감년도-시작년도 년
+		 				 0 개월
+		 }else if(마감월 < 시작월){
+		 	경력 = 마감년도-시작년도+1 년,
+		 		 (12-시작월)+마감월이 <6이면 그냥 년도만 셀렉
+		 		 				 >=6 이면 년도+1 셀렉
+		 }else{ //마감월 > 시작월
+		 	경력 = 마감년도-시작년도 년,
+		 		 마감월 - 시작월 이 <6이면 년도만 셀렉
+		 		 			 >=6이면 년도+1 셀렉
+		 }
+		 */
+		
+		// [4] 이력서 번호에 해당하는 학력 (education) 불러와서 학교명, 전공및 학위 출력해주기
+		
 		
 		return "company/matchupSearch";
 	}
