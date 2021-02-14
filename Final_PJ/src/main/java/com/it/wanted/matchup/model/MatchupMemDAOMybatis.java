098885e4cabfe6ba76766891531cfa5e02e798e3
jazzo <@jazzo5947@gmail.com>
2021-02-14 15:43:40 +0900

    전체목록보기, 찜한목록보기 뷰 및 기능 수정

--- Final_PJ/src/main/java/com/it/wanted/matchup/model/MatchupMemDAOMybatis.java
+++ Final_PJ/src/main/java/com/it/wanted/matchup/model/MatchupMemDAOMybatis.java
@@ -121,6 +121,7 @@ public class MatchupMemDAOMybatis implements MatchupMemDAO{
 		return sqlSession.selectList(namespace+"selectMcumemSearchList", mcuMemSearchVo);
 	}
 
+
 	@Override
 	public List<Map<String, Object>> selectZzimedList(MatchupMemSearchVO searchVo) {
 		return sqlSession.selectList(namespace+"selectZzimedList", searchVo);
